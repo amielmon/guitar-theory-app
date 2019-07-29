a0f5f81defc7c66cb7d6679eecfe14c55d199186
Amiel Monasterial <amielmon@gmail.com>
2019-07-29 16:52:12 -0400

    Some header stuff

--- guitar-theory-app/src/components/Header.js
+++ guitar-theory-app/src/components/Header.js
@@ -6,12 +6,9 @@ import { withRouter } from 'react-router'
 class Header extends Component {
     render() {
       return (
-        <div className="flex pa1 justify-between nowrap orange">
           <div className="flex flex-fixed black">
-            <div className="fw7 mr1">FretFriend</div>
-            <div className="ml1">|</div>
+            <div className="f1 lh-title avenir">FretFriend</div>
           </div>
-        </div>
       )
     }
   }
