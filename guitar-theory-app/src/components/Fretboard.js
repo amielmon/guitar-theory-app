f6a0302e58cebbe11a96b714d82270d3dbf66648
Amiel Monasterial <amielmon@gmail.com>
2019-08-08 14:40:09 -0400

    Style

--- guitar-theory-app/src/components/Fretboard.js
+++ guitar-theory-app/src/components/Fretboard.js
@@ -1,41 +1,35 @@
 import React, { Component } from "react";
 
-
 const Fret = () => (
-<div class="fl w-10 pa1">
-    <div class="outline bg-white o-10 tc pv3"></div>
+  <div class="fl w-10 pa1">
+    <div class="outline bg-white o-10 tc pv3" />
   </div>
 );
 
-
-
-
 class Fretboard extends Component {
-    render() {
-      return (
-        <div class="center h-50 w-80 bg-blue">
+  render() {
+    return (
+      <div class="center h-50 w-80 bg-blue">
         <div class="mw9 center ph3-ns">
-        <div class="flex flex-wrap cf ph2-ns">
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
-            <Fret/>
+          <div class="flex flex-wrap cf ph2-ns">
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+            <Fret />
+          </div>
         </div>
       </div>
-      
-       </div>
-      );
-    }
+    );
   }
-  
-  export default Fretboard;
-  
+}
+
+export default Fretboard;
