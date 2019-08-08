c8daa360e73f292eb4b33e295e5dbdf98aa648f3
Amiel Monasterial <amielmon@gmail.com>
2019-08-08 13:42:57 -0400

    center block for fretboard

--- guitar-theory-app/src/components/App.js
+++ guitar-theory-app/src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Fretboard from "./Fretboard";
 
 class App extends Component {
   render() {
     return (
       <div className="w-100">
         <Header />
-        <div className="vh-100 dt w-100 bg-dark-blue" />
+        <div className="flex items-center vh-100 dt w-100 bg-dark-blue">
+          <Fretboard/>
+        </div>
       </div>
     );
   }
---
+++ guitar-theory-app/src/components/Fretboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+
+
+class Fretboard extends Component {
+    render() {
+      return (
+        <div class="flex items-center center h-50 w-80 bg-blue">
+        </div>
+      );
+    }
+  }
+  
+  export default Fretboard;
+  
